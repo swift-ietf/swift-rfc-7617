f0271176c8ced189ae680a5b6cc8eeb2165cbc66
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 11:14:56 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
@@ -13,40 +13,64 @@ extension Target.Dependency {
 let package = Package(
     name: "swift-rfc-7617",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
     ],
     products: [
         .library(name: "RFC 7617", targets: ["RFC 7617"])
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-binary-serializer-primitives.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-serializer-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-binary-serializer-primitives.git",
+            branch: "main"
+        ),
+        .package(
+            url: "https://github.com/swift-primitives/swift-ascii-parser-primitives.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-incits/swift-incits-4-1986.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-binary-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-binary-primitives.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-ietf/swift-rfc-4648.git", branch: "main"),
-        .package(url: "https://github.com/swift-primitives/swift-parser-primitives.git", branch: "main")
+        .package(
+            url: "https://github.com/swift-primitives/swift-parser-primitives.git",
+            branch: "main"
+        ),
     ],
     targets: [
         .target(
             name: "RFC 7617",
             dependencies: [
-                .product(name: "ASCII Serializer Primitives", package: "swift-ascii-serializer-primitives"),
-                .product(name: "Binary Serializable Primitives", package: "swift-binary-serializer-primitives"),
-                .product(name: "Parseable ASCII Primitives", package: "swift-ascii-parser-primitives"),
+                .product(
+                    name: "ASCII Serializer Primitives",
+                    package: "swift-ascii-serializer-primitives"
+                ),
+                .product(
+                    name: "Binary Serializable Primitives",
+                    package: "swift-binary-serializer-primitives"
+                ),
+                .product(
+                    name: "Parseable ASCII Primitives",
+                    package: "swift-ascii-parser-primitives"
+                ),
                 .product(name: "INCITS 4 1986", package: "swift-incits-4-1986"),
                 .product(name: "Binary Primitives", package: "swift-binary-primitives"),
                 .product(name: "RFC 4648", package: "swift-rfc-4648"),
-                .product(name: "Parser Primitives", package: "swift-parser-primitives")
+                .product(name: "Parser Primitives", package: "swift-parser-primitives"),
             ]
         ),
         .testTarget(
             name: "RFC 7617 Tests",
             dependencies: [
-                "RFC 7617",
+                "RFC 7617"
             ]
         ),
     ],
